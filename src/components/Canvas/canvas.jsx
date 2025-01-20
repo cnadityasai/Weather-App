17d7a45144812406dbc8786e1e4aaee2d823893d
Aditya Sai <cnadityasai@Adityas-MacBook-Air.local>
2025-01-20 21:23:35 +0000

    Add fundamental canvas

--- src/components/Canvas/canvas.jsx
+++ src/components/Canvas/canvas.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./canvas.css";
 
 const canvas = () => {
-  return <div className="canvas">Canvas</div>;
+  return <div className="canvas">
+      <div className="contentBox">
+        <div id="cityName"> Chennai </div>
+        <div id="currTemp"> 24 C </div>
+      </div>
+    </div>;
 };
 
 export default canvas;
