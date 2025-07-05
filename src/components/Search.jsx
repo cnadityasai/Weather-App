30ee661a469128030977d71f8cd52be7e60d4457
Aditya Sai <cnadityasai@Adityas-MacBook-Air.local>
2025-07-05 19:30:33 +0530

    added suggestion functionality without styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Close from '../assets/close.png';
 import '../styles/Search.css'
 
 const Search = ({query, setQuery, onClose}) => {
 
+    const [suggestions, setSuggestions] = useState([])
+
+    const data = [
+        "London", "Chennai", "New York", "Seattle", "Bangalore",
+        "Ernakulam", "Hyderabad", "Mumbai", "Delhi", "New Delhi"
+    ]
+
+    function handleChange(e) {
+        setQuery(e.target.value)
+
+        const filtered = data.filter((item) => 
+            item.toLowerCase().startsWith(query.toLowerCase())
+        )
+
+        setSuggestions(filtered)
+
+    }
   return (
     <div className='search-container'>
         <input 
             type='text' 
             placeholder='Enter Location' 
             value={query} 
-            onChange={(e) => setQuery(e.target.value)} 
+            onChange={handleChange} 
         />
+        {
+            suggestions.length > 0 && (
+                <ul className="suggestions">
+                    {suggestions.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            )
+        }
         <img src={Close} alt='Close search bar' onClick={onClose}/>
     </div>
   )
